44f564d74ee0efd19657372ba583824cc9a3c257
nasschilik770 <nasschilik770@gmail.com>
2026-04-25 01:55:49 +0700

    fix

--- src/service/api-service.js
+++ src/service/api-service.js
@@ -1,7 +1,5 @@
 const API_URL = import.meta.env.VITE_API_URL;
 
-console.log(API_URL);
-
 async function getProducts() {
     try {
         const response = await fetch(`${API_URL}/get_barang.php`);
